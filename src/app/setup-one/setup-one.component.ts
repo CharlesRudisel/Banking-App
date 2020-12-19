9e45fafd2e8f73aac5df5d329c25c317dead8add
charles <charlesrudisel@gmail.com>
2020-12-19 02:35:07 -0500

    setup pages upgraded

--- src/app/setup-one/setup-one.component.ts
+++ src/app/setup-one/setup-one.component.ts
@@ -29,10 +29,10 @@ export class SetupOneComponent implements OnInit {
 
   ngOnInit(): void {
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 3; i++) {
       this.array.push(this.status)
     }
-    //console.log(this.array);
+    console.log(this.array);
   }
 
 }
--- src/app/setup-three/setup-three.component.ts
+++ src/app/setup-three/setup-three.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetupThreeComponent implements OnInit {
 
+  array = [];
+  status: boolean = true;
+  counter = 0;
+
+  
+
+  clickEvent(index:number){
+      //this.status = !this.status;   
+      this.array[index] = !this.array[index];  
+      if(this.array[index] == false){
+        this.counter += 1;
+      }
+      if(this.array[index] == true){
+        this.counter -= 1;
+      }
+      console.log(this.counter) ;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+
+    for (let i = 0; i < 39; i++) {
+      this.array.push(this.status)
+    }
+    //console.log(this.array);
   }
 
 }
